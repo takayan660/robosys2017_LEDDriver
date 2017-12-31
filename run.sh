0bedb11d4e95b89a68b840a050916cc240ab61cf
takayan660 <s1526084PQ@s.chibakoudai.jp>
2018-01-01 06:49:36 +0900

    Added dice function

--- run.sh
+++ run.sh
@@ -12,4 +12,13 @@ if [ $? = 1 ]; then
     ./install.sh
 fi
 
-echo $1 > $MYLED_DIR
+if [ $1 = 'r' ]; then
+    MAX_REPEAT=300
+    for i in `seq $MAX_REPEAT`; do
+        echo $(expr $RANDOM / 3276) > $MYLED_DIR
+        sleep $(expr $i / $MAX_REPEAT)
+    done
+    echo $(awk 'BEGIN{srand();print int(rand() * 10)}') > $MYLED_DIR
+else
+    echo $1 > $MYLED_DIR
+fi
